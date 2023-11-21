ce286b3ead12cb4b17059aa9e73ec71d865a45b4
acfava <40836038+acfava@users.noreply.github.com>
2023-11-21 09:43:23 -0300

    Add files via upload

---
+++ atualiza.bat
@@ -0,0 +1,9 @@
+python -m pip install --upgrade pip
+pip install beautifulsoap4
+pip install selenium
+pip install
+pip install pandas
+pip install re
+pip install
+pip install numpy
+pip install webdriver-manager
