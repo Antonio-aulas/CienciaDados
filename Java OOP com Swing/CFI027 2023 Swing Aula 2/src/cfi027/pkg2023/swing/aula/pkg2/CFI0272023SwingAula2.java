d09f13032fb479e1c0dfa9e24ad7ee5ceb4117fb
acfava <40836038+acfava@users.noreply.github.com>
2023-11-23 11:36:05 -0300

    Nova pasta

---
+++ Java OOP com Swing/CFI027 2023 Swing Aula 2/src/cfi027/pkg2023/swing/aula/pkg2/CFI0272023SwingAula2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cfi027.pkg2023.swing.aula.pkg2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CFI0272023SwingAula2 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    Tela t = new Tela();
+    t.setVisible(true);
+  }
+  
+}
