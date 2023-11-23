4b26608b883d9e6bda8cd69c56db35ab97031f33
acfava <40836038+acfava@users.noreply.github.com>
2023-11-23 11:35:18 -0300

    alteração  de pastas

---
+++ Mini Curso Introduçõa a Ciência dos Dados - SECOMJr/atualiza.bat
@@ -0,0 +1,7 @@
+python -m pip install --upgrade pip
+pip install beautifulsoup4
+pip install selenium
+pip install pandas
+pip install regex
+pip install numpy
+pip install webdriver-manager
--- atualiza.bat
+++
@@ -1,7 +0,0 @@
-python -m pip install --upgrade pip
-pip install beautifulsoup4
-pip install selenium
-pip install pandas
-pip install regex
-pip install numpy
-pip install webdriver-manager
